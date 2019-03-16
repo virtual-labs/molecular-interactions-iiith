cb41e451d8a67d40bf7cf107c638bd6565a54f46
somashekhar31 <17311a05r5@sreenidhi.edu.in>
2019-03-16 14:14:14 +0530

    changed7

--- src/lab/exp3/quiz.php
+++ src/lab/exp3/quiz.php
@@ -1,4 +1,4 @@
-<html>
+﻿<html>
 <head>
 <style type="text/css">
 *{
@@ -122,7 +122,7 @@ function getOptions(opt){
 	<?php 
 	if($_POST){
 		echo "</td><td>";
-		if($_POST['Q3'] == "A"){
+		if($_POST['Q3'] == "True"){
 			echo "<span style='color:green'>(".$_POST['Q3'].") IS CORRECT</span>";
 		}
 		else{
